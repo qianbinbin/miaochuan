c946c8699f69e823d631b10b2ede679abc061f20
Binbin Qian <qianbinbin@hotmail.com>
2021-08-13 13:28:55 +0800

    Fix redirection

--- miaochuan.sh
+++ miaochuan.sh
@@ -4,25 +4,25 @@ FILE="$1"
 HEAD_SIZE=$((256 * 1024))
 
 if [ ! -f "$FILE" ]; then
-  echo "usage: miaochuan [FILE]" && exit 1
+  echo "usage: miaochuan [FILE]" >&2 && exit 1
 fi
 
 SIZE=$(wc -c "$FILE" | awk '{ print $1 }')
 
 if [ "$SIZE" -lt $HEAD_SIZE ]; then
-  echo "[FILE] size should be >= 256 KiB" && exit 1
+  echo "[FILE] size should be >= 256 KiB" >&2 && exit 1
 fi
 
 result=
 
-if which md5sum >/dev/null; then
+if which md5sum >/dev/null 2>&1; then
   result=$(md5sum <"$FILE" | awk '{ print $1 }')
   result="$result#$(head -c $HEAD_SIZE "$FILE" | md5sum | awk '{ print $1 }')"
-elif which md5 >/dev/null; then
+elif which md5 >/dev/null 2>&1; then
   result=$(md5 <"$FILE")
   result="$result#$(head -c $HEAD_SIZE "$FILE" | md5)"
 else
-  echo "md5sum/md5 not found" && exit 1
+  echo "md5sum/md5 not found" >&2 && exit 1
 fi
 
 result="$result#$SIZE"
