d214f9d02985855fe820f77aec73291baf06c129
Binbin Qian <qianbinbin@hotmail.com>
2021-08-14 23:26:41 +0800

    Support globbing

--- README.md
+++ README.md
@@ -6,9 +6,14 @@
 
 ## 使用
 
+文件不要小于 256 KiB，支持通配符：
+
 ```sh
-$ ./miaochuan.sh ~/Downloads/ubuntu-20.04.2-live-server-amd64.iso
+$ miaochuan.sh ubuntu-20.04.2-live-server-amd64.iso
+aba7e22636c435c5008f5d059ae69a62#9473c90c734bd4406f4f8d452b9fd759#1215168512#ubuntu-20.04.2-live-server-amd64.iso
+$ miaochuan.sh *.iso
+e2ddc8268e4c164c32b4ba25be52c9af#4b75117359245f49840b1e5bce360f94#352321536#debian-10.4.0-amd64-netinst.iso
 aba7e22636c435c5008f5d059ae69a62#9473c90c734bd4406f4f8d452b9fd759#1215168512#ubuntu-20.04.2-live-server-amd64.iso
 ```
 
-文件不要小于 256 KiB。
+秒传代码会输出到 stdout，其他信息会重定向到 stderr。
--- miaochuan.sh
+++ miaochuan.sh
@@ -1,31 +1,59 @@
 #!/usr/bin/env sh
 
-FILE="$1"
 HEAD_SIZE=$((256 * 1024))
 
-if [ ! -f "$FILE" ]; then
-  echo "usage: miaochuan [FILE]" >&2 && exit 1
+error() {
+  echo "$@" >&2
+}
+
+if [ $# -eq 0 ]; then
+  error "Usage: miaochuan FILE ..."
+  exit 2
 fi
 
-SIZE=$(wc -c "$FILE" | awk '{ print $1 }')
+exist() {
+  which "$1" >/dev/null 2>&1
+}
 
-if [ "$SIZE" -lt $HEAD_SIZE ]; then
-  echo "[FILE] size should be >= 256 KiB" >&2 && exit 1
+if ! exist md5sum && ! exist md5; then
+  error "miaochuan: md5sum/md5 not found"
+  exit 127
 fi
 
-result=
+do_md5() {
+  if exist md5sum; then
+    md5sum | awk '{ print $1 }'
+  elif exist md5; then
+    md5
+  else
+    error "miaochuan: Van!shment  Th!s World!!"
+    exit 127
+  fi
+}
 
-if which md5sum >/dev/null 2>&1; then
-  result=$(md5sum <"$FILE" | awk '{ print $1 }')
-  result="$result#$(head -c $HEAD_SIZE "$FILE" | md5sum | awk '{ print $1 }')"
-elif which md5 >/dev/null 2>&1; then
-  result=$(md5 <"$FILE")
-  result="$result#$(head -c $HEAD_SIZE "$FILE" | md5)"
-else
-  echo "md5sum/md5 not found" >&2 && exit 1
-fi
+mc() {
+  file="$1"
 
-result="$result#$SIZE"
-result="$result#$(basename "$FILE")"
+  if [ ! -f "$file" ] || [ ! -r "$file" ]; then
+    error "miaochuan: $file: Cannot access"
+    return 1
+  fi
 
-echo "$result"
+  size=$(wc -c "$file" | awk '{ print $1 }')
+
+  if [ "$size" -lt $HEAD_SIZE ]; then
+    error "miaochuan: $file: Size must be >= 256 KiB"
+    return 1
+  fi
+
+  result=$(do_md5 <"$file")
+  result="$result#$(head -c $HEAD_SIZE "$file" | do_md5)"
+  result="$result#$size"
+  result="$result#$(basename "$file")"
+
+  echo "$result"
+}
+
+for file in "$@"; do
+  mc "$file"
+done
