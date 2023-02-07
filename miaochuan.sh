83c21d2102a9b0741e94a6f6f7ef0829d1d4d3c4
Binbin Qian <qianbinbin@hotmail.com>
2023-02-08 01:48:37 +0800

    Support folders #1

--- miaochuan.sh
+++ miaochuan.sh
@@ -1,22 +1,49 @@
 #!/usr/bin/env sh
 
-error() {
-  echo "$@" >&2
-}
+unset KEEP_PATH CD
 
-if [ $# -eq 0 ]; then
-  error "Usage: miaochuan FILE ..."
-  exit 2
-fi
+error() { echo "$@" >&2; }
 
-exist() {
-  which "$1" >/dev/null 2>&1
-}
+exist() { command -v "$1" >/dev/null 2>&1; }
 
 if ! exist md5sum && ! exist md5; then
   error "miaochuan: md5sum/md5 not found"
   exit 127
 fi
+
+USAGE=$(
+  cat <<-END
+Usage: miaochuan [OPTION]... FILE...
+
+  -k    keep relative path(s)
+  -c    change to directory DIR
+  -h    display this help and exit
+
+Home page: <https://github.com/qianbinbin/miaochuan>
+END
+)
+
+_exit() {
+  error "$USAGE"
+  exit 2
+}
+
+while getopts "khc:" c; do
+  case $c in
+  k) KEEP_PATH=true ;;
+  c) CD=$OPTARG ;;
+  h) error "$USAGE" && exit ;;
+  *) _exit ;;
+  esac
+done
+
+if [ -n "$CD" ]; then
+  cd "$CD" || exit 1
+fi
+
+shift $((OPTIND - 1))
+
+[ $# -eq 0 ] && _exit
 
 do_md5() {
   if exist md5sum; then
@@ -30,22 +57,27 @@ do_md5() {
 }
 
 mc() {
-  file="$1"
+  f=$(realpath --relative-to "$PWD" "$1")
 
-  if [ ! -f "$file" ] || [ ! -r "$file" ]; then
-    error "miaochuan: $file: Cannot access"
+  if [ ! -f "$f" ] || [ ! -r "$f" ]; then
+    error "miaochuan: $f: Cannot access"
     return 1
   fi
 
-  size=$(wc -c "$file" | awk '{ print $1 }')
+  size=$(wc -c "$f" | awk '{ print $1 }')
 
-  result=$(do_md5 <"$file")
+  result=$(do_md5 <"$f")
   result="$result#$size"
-  result="$result#$(basename "$file")"
+  if [ "$KEEP_PATH" = true ]; then
+    _f="$f"
+  else
+    _f=$(basename "$f")
+  fi
+  result="$result#$_f"
 
   echo "$result"
 }
 
-for file in "$@"; do
+find "$@" -type f | while IFS= read -r file; do
   mc "$file"
 done
